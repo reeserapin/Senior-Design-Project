3e0768741126f7245c13fd8a2f719452ba57bfe4
Reese Rapin <reeserapin@Reeses-MBP.home>
2024-09-08 21:08:59 -0400

    prof bio for reese

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Senior-Design-Project
+
+# INSERT_PROJECT_NAME
+
+> Reese Rapin (rapinre@mail.uc.edu), CS<br>
+> Minha Raza (razamh@mail.uc.edu), CS<br>
+> Neha Annareddy (annarenr@mail.uc.edu), CS<br>
+> Ayush Sunkad (sunkadah@mail.uc.edu), CS<br>
+
+> Advised by INSERT_PROFESSORS_NAME PhD (INSERT_PROFESSORS_EMAIL), Department of CS
+
+## Topic
+
+Social media app for pets - As a foster parent, I often wonder what happens to animals after they're adopted, so this platform would allow the pet’s account to transfer to the new owner while fostering families could still follow their journey. It would also help make pets more adoptable, as people could join and see photos of available animals. For breeders, the app could include a verification process to prevent scams, with ID checks and multiple pet photos. A family tree feature could track a pet’s lineage—parents, siblings, etc.—and help identify ethical breeders.
---
+++ professional_biographies/Reese_Rapin.mk
@@ -0,0 +1,43 @@
+# Senior-Design-Professional-Biography
+## NAME
+Reese Rapin
+
+## CONTACT INFO
+School Email: rapinre@mail.uc.edu
+LinkedIn: https://www.linkedin.com/in/reese-rapin/
+
+## CO-OP AND OTHER RELATED
+
+**Johnson & Johnson MedTech, Ethicon**, Cincinnati, OH  
+**Electrical Engineer**  
+*January 2024 – April 2024 (Co-op/Full-time)*  
+- Spearheaded the design of multiple printed circuit boards in KiCad, showcasing schematic creation and layout design  
+- Developed code for communicating with I2C and SPI  
+- Automated labor-intensive tasks by crafting efficient test scripts using Squish and Python, significantly expediting project timelines and enhancing overall workflow efficiency  
+
+**GE Appliances, a Haier company**, Louisville, KY  
+**Embedded Software Engineer on the Laundry Dryer Team**  
+*May 2023 – August 2023 (Co-op/Full-time)*  
+- Performed bench testing on a load box utilizing a J-Link tool to validate and verify embedded software functionality  
+- Developed and implemented modules that allowed system engineers to efficiently write and manage data within the system, enhancing the overall functionality and usability  
+- Served as Co-op Ambassador on the Women in Technology Executive Board, actively promoting and advocating for gender diversity and inclusion in the technology field  
+
+**Software Engineer on the Android on Appliances Team**  
+*August 2022 – December 2022 (Co-op/Full-time)*  
+- Utilized Flutter/Dart to develop code for UI/UX on LCD-controlled appliances such as the wall oven  
+- Proactively participated with designers in translating mockups and wireframes to adhere to touchscreen specifications  
+- Implemented CSS, Node.js, and Electron to create a dark mode for one of GEA’s desktop applications  
+
+**Embedded Software Engineer on the Service Diagnostics Team**  
+*January 2022 – May 2022 (Co-op/Full-time)*  
+- Used Test Driven Development to program on MacOS in Visual Studio Code with JavaScript, C, and Lua  
+- Modified and developed new REPL commands and added Jasmine Tests for Service Scripts tool which configures software update devices  
+- Built a Micro Sumo Fighting Robot in accordance with Unified Sumo Robot Rules
+
+**Skills/expertise areas**   
+- C, C++, Git, GitHub Actions, Python, Lua, Dart, Flutter, SQL, VBA, LabVIEW, Node.js, MATLAB, Soldering, Android Studio, Squish, KiCad,
+
+## AREAS OF INTEREST/TYPE OF PROJECT SOUGHT
+- Embedded Development
+- Mobile Apps : Social Media app for Pets.
+- Website
